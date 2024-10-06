01b363ddceb8e63f11ea3561084c981fa50fa44c
hothanhnghia-nlu <nghiant369@gmail.com>
2024-10-06 22:57:37 +0700

    Initial project

---
+++ js/sources.js
@@ -0,0 +1,39 @@
+var CoDen = {
+	Tot: 'images/chessPieces/Tot_Den.png',
+	Tuong: 'images/chessPieces/Tuong_Den.png',
+	Ma: 'images/chessPieces/Ma_Den.png',
+	Xe: 'images/chessPieces/Xe_Den.png',
+	Hau: 'images/chessPieces/Hau_Den.png',
+	Vua: 'images/chessPieces/Vua_Den.png',
+	Rong: "images/chessPieces/Rong.png",
+	// Name
+	Tot_Name: 'Tot_Den',
+	Tuong_Name: 'Tuong_Den',
+	Ma_Name: 'Ma_Den',
+	Xe_Name: 'Xe_Den',
+	Hau_Name: 'Hau_Den',
+	Vua_Name: 'Vua_Den'
+}
+
+var CoDo = {
+	Tot: 'images/chessPieces/Tot_Do.png',
+	Tuong: 'images/chessPieces/Tuong_Do.png',
+	Ma: 'images/chessPieces/Ma_Do.png',
+	Xe: 'images/chessPieces/Xe_Do.png',
+	Hau: 'images/chessPieces/Hau_Do.png',
+	Vua: 'images/chessPieces/Vua_Do.png',
+	Rong: "images/chessPieces/Rong.png",
+	// Name
+	Tot_Name: 'Tot_Do',
+	Tuong_Name: 'Tuong_Do',
+	Ma_Name: 'Ma_Do',
+	Xe_Name: 'Xe_Do',
+	Hau_Name: 'Hau_Do',
+	Vua_Name: 'Vua_Do'
+}
+
+var Mau = {
+	Trang: "255,255,255",
+	Den: "170,182,155",
+	NuocDi: "246,205,97"
+}
